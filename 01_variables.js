5191c0c186b9e29439b2e0118617c438c0cce2ea
SHUBH-CODE-art <83896948+SHUBH-CODE-art@users.noreply.github.com>
2024-07-02 21:58:57 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 123422
+let accountEmail = "hitesh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+/*  accountId =2 */
+accountEmail = "hc@gmail.com"
+accountPassword = "129800"
+accountCity = "Pune"
+
+console.log(accountId);
+
+console.log([accountId,accountEmail,accountPassword,accountCity])
